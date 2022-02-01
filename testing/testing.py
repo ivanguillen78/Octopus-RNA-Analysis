c5abbb0a29516caca8756505b4c5aa8565d9a47a
Ivan Guillen <ivanguillen@Ivans-Mac-mini.local>
2022-01-31 19:22:17 -0800

    sorted list of sequences according to id

--- testing/testing.py
+++ testing/testing.py
@@ -5,29 +5,23 @@ import pandas as pd
 seq_list = []
 edit_list = []
 
-# with open("../data/swissprotORF.fasta") as fasta_file:
-#     parser = fastaparser.Reader(fasta_file)
-#     for seq in parser:
-#         seq_list.append({'id': seq.id, 'seq': seq.sequence_as_string()})
+with open("../data/swissprotORF.fasta") as fasta_file:
+    parser = fastaparser.Reader(fasta_file)
+    for seq in parser:
+        seq_list.append({'id': seq.id, 'seq': seq.sequence_as_string()})
 
 
-with open("test.csv", newline='') as csvfile:
+with open("../data/aes_profile.csv", newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         edit_list.append({'id': row['orf'], 'pos': [int(row['pos'])]})
 
-df = pd.DataFrame(edit_list)
-print(type(df['id'][0]))
-# for x in edit_list:
-#     for y in seq_list:
-#         if x['id'] == y['id']:
-#             x['seq'] = y['seq']
+sortedList = sorted(edit_list, key=lambda x: x['id'])
 
-# for i in range(len(edit_list)):
-#     if (edit_list[i]['id'] == edit_list[i-1]['id']):
-#         edit_list[i]['flag'] = 'duplicate'
-#         #del edit_list[i-1]
+for x in sortedList:
+    for y in seq_list:
+        if x['id'] == y['id']:
+            x['seq'] = y['seq']
 
-
-# for i in range(len(edit_list)):
-#     print(edit_list[i])
+for i in range(20):
+    print(sortedList[i])
