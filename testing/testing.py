9fb27c8d8f492dd3bccf8ce9c7842ce5bea5864d
Ivan Guillen <ivanjguillen78@gmail.com>
2022-02-15 22:34:24 -0800

    testing

--- testing/testing.py
+++ testing/testing.py
@@ -1,14 +1,14 @@
-import fastaparser
 import csv
+import fastaparser
 from Bio.Seq import Seq
 
 edit_dict = {}
 score_dict = {}
 
-with open("../data/aes_profile.csv", newline='') as csvfile:
+with open("../data/aes_profile.csv", newline='', encoding="utf8") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        if (row['orf'] not in edit_dict):
+        if row['orf'] not in edit_dict:
             edit_dict[row['orf']] = [int(row['pos'])]
         else:
             edit_dict[row['orf']].append(int(row['pos']))
@@ -19,22 +19,22 @@ def secondary_structure(pos_list, sequence):
         lo = pos
         hi = pos + 1
         for i in range(len(sequence)):
-           rev_comp = str(Seq(sequence[lo:hi]).reverse_complement())
-           if (rev_comp not in sequence):
-               break
-           if ((rev_comp in sequence) and (rev_comp not in sequence[lo:hi])):
-               lo -= 1
-               hi += 1
+            rev_comp = str(Seq(sequence[lo:hi]).reverse_complement())
+            if rev_comp not in sequence:
+                break
+            if ((rev_comp in sequence) and (rev_comp not in sequence[lo:hi])):
+                lo -= 1
+                hi += 1
         len_list.append(len(rev_comp))
     return len_list
 
-with open("../data/swissprotORF.fasta") as fasta_file:
+with open("../data/swissprotORF.fasta", encoding="utf8") as fasta_file:
     parser = fastaparser.Reader(fasta_file)
     for seq in parser:
-        if (seq.id in edit_dict):
+        if seq.id in edit_dict:
             score_dict[seq.id] = secondary_structure(edit_dict[seq.id], seq.sequence_as_string())
 
-with open('score_list.csv', 'w', newline='') as csvfile:
+with open('score_list.csv', 'w', newline='', encoding="utf8") as csvfile:
     fieldnames = ['id', 'score']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
