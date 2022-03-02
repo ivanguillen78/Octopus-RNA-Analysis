4e44b26c67d239e7729c5ff160659ae462e64987
Ivan Guillen <ivanjguillen78@gmail.com>
2022-03-01 23:46:40 -0800

    another one

--- src/main/secondary_structure.py
+++ src/main/secondary_structure.py
@@ -44,13 +44,13 @@ def secondary_structure(pos_list, sequence):
         left, right = pos, pos
         count = 0
         while count < 100:
-            new_str = sequence[:left] + sequence[right + 1 :]
+            new_str = sequence[:left] + sequence[right + 1 :]  # noqa: E203
             rev_comp = str(Seq(sequence[lo_:hi_]).reverse_complement())
-            if (str(Seq(sequence[lo_ - 1 : hi_]).reverse_complement()) in new_str) and (
+            if (str(Seq(sequence[lo_ - 1 : hi_]).reverse_complement()) in new_str) and (  # noqa: E203
                 lo_ > 0
             ):
                 lo_ -= 1
-            if (str(Seq(sequence[lo_ : hi_ + 1]).reverse_complement()) in new_str) and (
+            if (str(Seq(sequence[lo_ : hi_ + 1]).reverse_complement()) in new_str) and (  # noqa: E203
                 hi_ < len(sequence)
             ):
                 hi_ += 1
